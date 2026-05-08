1a5756337b52456118ce3debff39f7fff448f79f
prince-cloud <acheampongprince227@gmail.com>
2026-05-08 08:26:19 +0000

    update: updated requirements.txt

--- core/models.py
+++ core/models.py
@@ -5,6 +5,111 @@ from configuration.models import Branch, Device, Service
 
 
 # ---------------------------------------------------------------------------
+# Ticket Modeling
+# ---------------------------------------------------------------------------
+# class TicketStatus(models.Model):
+#     WAITING = "Waiting"
+#     ON_GOING = "On Going"
+#     ON_HOLD = "On Hold"
+#     COMPLETED = "Completed"
+#     CANCLLED = "Cancelled"
+#     SKIPPED = "Skipped"
+
+
+# class Ticket(models.Model):
+
+#     branch = models.ForeignKey(
+#         Branch,
+#         related_name="tickets",
+#         null=True,
+#         on_delete=models.SET_NULL,
+#     )
+#     assigned_to = models.ForeignKey(
+#         CustomUser,
+#         related_name="tickets_assigned",
+#         null=True,
+#         on_delete=models.SET_NULL,
+#     )
+#     customer = models.ForeignKey(
+#         Customer,
+#         on_delete=models.SET_NULL,
+#         null=True,
+#         related_name="tickets",
+#     )
+
+#     ticket_number = models.CharField(max_length=20)
+
+#     ticket_audio = models.FileField(upload_to="ticket_audio/", null=True, blank=True)
+#     signature = models.ImageField(upload_to="ticket_signatures/", null=True, blank=True)
+
+#     waiting_time = models.PositiveIntegerField(
+#         null=True,
+#         blank=True,
+#         help_text="Waiting time (in seconds)",
+#         default=0,
+#     )
+#     called_time = models.DateTimeField(
+#         null=True,
+#         blank=True,
+#         help_text="Time ticket was called",
+#     )
+#     start_serve_time = models.DateTimeField(
+#         null=True,
+#         blank=True,
+#         help_text="Time ticket was started to be served",
+#     )
+#     served_time = models.PositiveIntegerField(
+#         null=True,
+#         blank=True,
+#         help_text="Serving time (in seconds)",
+#         default=0,
+#     )
+#     total_time_spent = models.PositiveIntegerField(
+#         null=True,
+#         blank=True,
+#         help_text="Total Time spent at branch (in seconds)",
+#         default=0,
+#     )
+
+#     counter = models.ForeignKey(
+#         Counter,
+#         related_name="tickets_served",
+#         on_delete=models.SET_NULL,
+#         null=True,
+#         blank=True,
+#     )
+
+#     status = models.CharField(
+#         choices=Status.choices,
+#         max_length=50,
+#         default=Status.WAITING,
+#     )
+
+#     hold_reason = models.CharField(
+#         max_length=300,
+#         null=True,
+#         blank=True,
+#     )
+
+#     from_device = models.ForeignKey(
+#         Device,
+#         on_delete=models.SET_NULL,
+#         null=True,
+#         blank=True,
+#     )
+
+#     uuid = models.UUIDField(default=uuid4, unique=True, editable=False)
+#     created_at = models.DateTimeField(auto_now_add=True)
+#     updated_at = models.DateTimeField(auto_now=True)
+
+#     def __str__(self):
+#         return "{} - {}".format(
+#             date=self.created_at,
+#             ticket_number=self.ticket_number,
+#         )
+
+
+# ---------------------------------------------------------------------------
 # Deposit tickets
 # ---------------------------------------------------------------------------
 
